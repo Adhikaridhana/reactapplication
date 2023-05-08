9f0b492dba03b753b696f736acaea66d57069d84
A.Dhana lakshmi <adhikaridhana1998@gmail.com>
2023-05-08 21:47:24 +0530

    first commit

---
+++ src/components/menu.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState } from 'react'
+import Link from 'next/link'
+
+export const Menu = () => {
+  const [menuItem,setMenuItem]=useState('')
+  useEffect(()=>{
+    let pathname = window.location.pathname;
+    setMenuItem(pathname.slice(1));
+  })
+  const fnMenuClick=(eve)=>{
+    setMenuItem(eve.target.id);
+  }
+  return (
+    <ul className="menu">
+      <li >
+        <Link id='home' className={menuItem=='home' && 'active-menu'} onClick={fnMenuClick} href="/home">Home</Link>
+      </li>
+      <li>
+        <Link id='about' className={menuItem=='about' && 'active-menu'} onClick={fnMenuClick} href="/about">About Us</Link>
+      </li>
+      <li>
+        <Link id='contact' className={menuItem=='contact' && 'active-menu'} onClick={fnMenuClick} href="/contact">Contact</Link>
+      </li>
+    </ul>
+  )
+}
